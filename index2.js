da5e972f0bfe17f366cf3228c872730eec5edee8
Felipe Herrera <83690907+casafchile@users.noreply.github.com>
2024-07-11 18:51:35 -0400

    Create index2.js

---
+++ index2.js
@@ -0,0 +1,11 @@
+class Colorful extends React.Component {
+  render() { 
+    return (
+      <div style={{
+                  color: "purple",
+                  fontSize: 40, 
+                  border: "2px solid purple"
+                  }}>Style Me!</div>
+    );
+  }
+};
